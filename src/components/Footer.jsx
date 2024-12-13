dcfd0cc0191aa88e6d8a68cd66cbcc9741e0352d
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-12-13 12:54:30 +0100

    fix: fix the code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import style from "./Footer.module.css"
 function Footer() {
     return (
         <footer>
-            <p className="container pt-3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores possimus porro quam, libero minus atque similique quaerat voluptatum est at praesentium repudiandae dolor aliquam voluptatem? Dignissimos unde atque odit quidem.
+            <p className="container">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores possimus porro quam, libero minus atque similique quaerat voluptatum est at praesentium repudiandae dolor aliquam voluptatem?
             </p>
         </footer>
     );
